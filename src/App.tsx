1272553c196deb3a7e5ea4aa2422b8e7092d2b52
Bruno-rasq <116691516+Bruno-rasq@users.noreply.github.com>
2024-02-10 03:22:53 +0000

    Commit: aula 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
 import './App.css';
+import Logo from './assets/logo-nlw-expert.svg';
+import {NoteCard} from './components/note-card';
+import {NewNoteCard} from './components/new-note-card';
 
 export function App() {
    return (
-  
-      <h1 className="
-        text-3xl 
-        font-bold 
-        underline"
-      >
-        Hello, World!
-      </h1>
+     <div className="
+       mx-auto 
+       max-w-6xl 
+       my-12 
+       space-y-6">
+       <img src = { Logo } alt="nlw Expert"/>
+       
+       <form className="w-full">
+         <input 
+           type = "text" 
+           placeholder="Busque em suas notas..." 
+           className="
+           w-full 
+           bg-transparent 
+           text-3xl 
+           font-semibold 
+           tracking-tight 
+           outline-none 
+           placeholder:text-slate-500"
+           />
+       </form>
 
+       <div className="h-px bg-slate-700"/>
+
+
+       <div className="
+         grid 
+         grid-cols-3 
+         auto-rows-[250px] 
+         gap-6">
+         
+
+         <NewNoteCard/>
+
+         <NoteCard/>
+         <NoteCard/>
+         <NoteCard/>
+         <NoteCard/>
+         <NoteCard/>
+
+         
+       </div>
+     </div>
   )
 }
